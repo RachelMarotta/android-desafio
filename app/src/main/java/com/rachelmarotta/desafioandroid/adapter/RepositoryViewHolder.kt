a2ba644e33d4ae206d3d3a37287e61bb4fcd9da4
RachelMarotta <kelmarotta@icloud.com>
2021-05-28 09:25:04 -0300

    add glid

--- app/src/main/java/com/rachelmarotta/desafioandroid/adapter/RepositoryViewHolder.kt
+++ app/src/main/java/com/rachelmarotta/desafioandroid/adapter/RepositoryViewHolder.kt
@@ -1,14 +1,16 @@
 package com.rachelmarotta.desafioandroid.adapter
 
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.rachelmarotta.desafioandroid.R
 import com.rachelmarotta.desafioandroid.model.Item
 
 class RepositoryViewHolder(listView: View) : RecyclerView.ViewHolder(listView) {
 
-//    private val repositoryPhoto: TextView = listView.findViewById(R.id.repository_photo)
+    private val repositoryPhoto: ImageView = listView.findViewById(R.id.repository_photo)
     private val repositoryName: TextView = listView.findViewById(R.id.repository_name)
     private val repositoryDescription: TextView = listView.findViewById(R.id.repository_description)
     private val repositoryAuthor: TextView = listView.findViewById(R.id.repository_author)
@@ -16,6 +18,7 @@ class RepositoryViewHolder(listView: View) : RecyclerView.ViewHolder(listView) {
     private val repositoryForks: TextView = listView.findViewById(R.id.repository_forks)
 
     fun bind(item: Item) {
+        Glide.with(itemView.context).load(item.owner.avatar_url).into(repositoryPhoto)
         repositoryName.text = item.name
         repositoryDescription.text = item.description
         repositoryAuthor.text = item.owner.login
